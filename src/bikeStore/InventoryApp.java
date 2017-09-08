1c5716ad0676609f320bc63970307894236dda90
Ivan Garcia <igtech03@gmail.com>
2017-09-07 21:12:59 -0600

    Add exception handling for year when creating a bicycle

--- src/bikeStore/InventoryApp.java
+++ src/bikeStore/InventoryApp.java
@@ -2,39 +2,50 @@ package bikeStore;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InventoryApp {
 
 	private static Scanner scan = new Scanner(System.in);
 	private static Inventory inv = new Inventory("Bikes");
-	
+
 	private static void displayBicycles() {
-		for (Bicycle element : inv.getBicycles()) {
-			System.out.println(element);
+		if (inv.getBicycles().isEmpty()) {
+			System.out.printf("There are no bicycles to list");
+		} else {
+			inv.getBicycles().forEach(bicycle -> System.out.printf("%n%s", bicycle));
+			System.out.printf("%n%n");
+		}
+	}
+
+	private static void addBicycle() {
+		int year = 0;
+		String make, model;
+		try {
+			System.out.println("Year: ");
+			year = scan.nextInt();
+			System.out.println("Make: ");
+			scan.nextLine();
+			make = scan.nextLine();
+			System.out.println("Model: ");
+			model = scan.nextLine();
+			inv.add(new Bicycle(year, make, model));
+		} catch (InputMismatchException e) {
+			System.out.printf("ERROR: Input is not a valid year. Please try again %ne.g 2017%n");
+			scan.nextLine();
 		}
 		System.out.println();
 	}
-	
-	private static void addBicycle() {
-		System.out.println("Year: ");
-		int a = scan.nextInt();
-		System.out.println("Make: ");
-		scan.nextLine();
-		String b = scan.nextLine();
-		System.out.println("Model: ");
-		String c = scan.nextLine();
-		inv.add(new Bicycle(a, b, c));
-		System.out.println();
-	}
-	
+
 	/**
-	 * Removes a bicycle from the ArrayList<Bicycle> from an id. 
-	 * If the id does not exist it returns not found
+	 * Removes a bicycle from the ArrayList<Bicycle> from an id. If the id does not
+	 * exist it returns not found
+	 * 
 	 * @param bicycles
 	 * @param bikeId
 	 */
-	
+
 	private static void remove(ArrayList<Bicycle> bicycles, int bikeId) {
 		int index = inv.bicycleSearch(bicycles, bikeId);
 		if (index >= 0) {
@@ -45,10 +56,10 @@ public class InventoryApp {
 			System.out.printf("Bicycle Id: \"%d\" not found %n%n", bikeId);
 		}
 	}
-	
+
 	/**
-	 * searches the ArrayList<Bicycle> from an id. 
-	 * If the id does not exist it returns not found.
+	 * searches the ArrayList<Bicycle> from an id. If the id does not exist it
+	 * returns not found.
 	 * 
 	 * @param bicycles
 	 * @param bikeId
@@ -61,43 +72,37 @@ public class InventoryApp {
 			System.out.printf("Bicycle with Id: \"%d\" not found %n", bikeId);
 		}
 	}
-	
+
 	private static void searchBicycleById() {
 		System.out.println("Find Bicycle with Id: ");
 		int findBikeId = scan.nextInt();
 		find(inv.getBicycles(), findBikeId);
-		System.out.println("");
+
 	}
-	
-	private static void removeBicycleById(){
+
+	private static void removeBicycleById() {
 		System.out.println("Remove Bicycle with Id: ");
 		int removeBikeId = scan.nextInt();
 		remove(inv.getBicycles(), removeBikeId);
 	}
-	
+
 	private static void bicycleCount() {
 		System.out.printf("Number of Bicycles: %s%n%n", inv.getBicycles().size());
 	}
-		
+
 	private static void throwErrFromSelection() {
 		System.out.println("Error: Enter a selection 1 - 6\n");
 	}
-	
-	private static void mainMenu() {
-		inv.add(new Bicycle(1974, "Schwinn", "Le Tour"));
-		inv.add(new Bicycle(1975, "Huffy", "Thunder Road"));
-		inv.add(new Bicycle(1979, "Cornelo", "Super"));
 
+	private static void mainMenu() {
 		int menu = 0;
 		while (menu != 6) {
 			ArrayList<String> theMenu;
-			theMenu = new ArrayList<String>(Arrays.asList(
-					"1. Display all items", "2. Add an item", "3. Find an item", 
-					"4. Delete an item", "5. Number of items", "6. Exit", 
-					"Enter your selection: "));
-			for (String s : theMenu) {
-		         System.out.print(s + "\n");
-		    }
+			theMenu = new ArrayList<String>(
+					Arrays.asList("1. Display all bicycles", "2. Add a bicycle", "3. Find a bicycle",
+							"4. Delete a bicycle", "5. Number of bicycles", "6. Exit", "Enter your selection: "));
+			theMenu.forEach(opt -> System.out.printf("%s%n", opt));
+
 			menu = scan.nextInt();
 
 			switch (menu) {
@@ -117,19 +122,22 @@ public class InventoryApp {
 				bicycleCount();
 				break;
 			case 6:
+				System.out.println("Goodbye");
+				scan.close();
 				break;
 			default:
 				throwErrFromSelection();
 				break;
 			}
 		}
-		
-		System.out.println("Goodbye");
-		scan.close();
-		
 	}
 
 	public static void main(String[] args) {
+		inv.add(new Bicycle(1974, "Schwinn", "Le Tour"));
+		inv.add(new Bicycle(1975, "Huffy", "Thunder Road"));
+		inv.add(new Bicycle(1979, "Cornelo", "Super"));
+		inv.add(new Bicycle(1983, "SE", "Racing Quadangle"));
+		
 		mainMenu();
 	}
 }
